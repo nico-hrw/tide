80d25d773f40ff12b2b10854a3bb4ace8ec12b4e
Nico Heerwagen <nico.heerwagen.mail@gmail.com>
2026-05-18 18:22:06 +0200

    fix: change canvas note icon from teal to grey

--- web/src/components/Layout/Sidebar.tsx
+++ web/src/components/Layout/Sidebar.tsx
@@ -825,7 +825,7 @@ const FileItem = ({ file, level, onSelect, onDelete, onRename, onVisibility, onS
                 {file.title.startsWith('#')
                     ? <Lock size={15} className="shrink-0 text-gray-400" />
                     : file.type === 'canvas'
-                        ? <Layout size={15} className="shrink-0 text-teal-400" />
+                        ? <Layout size={15} className="shrink-0 text-gray-400" />
                         : <FileText size={15} className="shrink-0 text-gray-400" />}
                 {editingId === file.id ? (
                     <input
